94f84d23b3e7e89b6d3b939e3a1e418eba989661
Phun Peeticharoenthum <phunphun484@gmail.com>
2025-03-10 06:13:17 +0700

    EDIT: Improve type safety for FromConfig

--- Runtime/FromConfig.cs
+++ Runtime/FromConfig.cs
@@ -13,10 +13,15 @@ namespace PhEngine.QuickDropdown
         public override bool CheckInvalid(out Exception exception)
         {
             exception = null;
+            if (ConfigType == typeof(ScriptableGroup))
+            {
+                exception = new Exception("Config cannot be a type of ScriptableGroup");
+                return true;
+            }
             if (ConfigType.IsSubclassOf(typeof(ScriptableContainer))) 
                 return base.CheckInvalid(out exception);
             
-            exception = new Exception("The type must be a subclass of ScriptableGroup.");
+            exception = new Exception("The type must be a subclass of ScriptableContainer.");
             return true;
         }
     }
