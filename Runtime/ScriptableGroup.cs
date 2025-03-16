0f5bb082be24deb78807768b34634c22c4a041bf
Phun Peeticharoenthum <phunphun484@gmail.com>
2025-03-17 01:55:57 +0700

    EDIT: Fix nested display for ScriptableGroup

--- Runtime/ScriptableGroup.cs
+++ Runtime/ScriptableGroup.cs
@@ -9,25 +9,21 @@ namespace PhEngine.QuickDropdown
     {
         public override string[] GetStringOptions(Type type)
         {
-            return GetOptions(type);
+            return GetOptions(type, new List<ScriptableGroup>(){this});
         }
 
-        string[] GetOptions(Type type, List<ScriptableGroup> parentList = null)
+        string[] GetOptions(Type type, List<ScriptableGroup> parentList)
         {
-            parentList ??= new List<ScriptableGroup>();
             return ElementList
                 .SelectMany(so=>
                 {
-                    if (so == this)
-                        return new string[] { };
-                    
                     if (so is ScriptableGroup nestedGroup)
                     {
                         if (parentList.Contains(nestedGroup))
                             return new string[] { };
 
                         var newParentList = new List<ScriptableGroup>(parentList);
-                        newParentList.Add(this);
+                        newParentList.Add(nestedGroup);
                         return nestedGroup.GetOptions(type, newParentList).ToArray();
                     }
                     return so && so.GetType() == type ? new [] {GetOptionString(so)} : new string[]{};
@@ -36,32 +32,29 @@ namespace PhEngine.QuickDropdown
 
             string GetOptionString(Object obj)
             {
-                return parentList.Count <= 0 ? 
+                return parentList.Count <= 1 ? 
                     obj.name : 
-                    string.Join("/", parentList.Select(p => p.name)) + "/" + obj.name;
+                    string.Join("/", parentList.Skip(1).Select(p => p.name)) + "/" + obj.name;
             }
         }
 
         public override Object GetObjectFromFlatTree(Type type, int targetIndex)
         {
             var currentIndex = 0;
-            return FlattenAndReturn(new List<ScriptableGroup>(), type, ref currentIndex, targetIndex);
+            return FlattenAndReturn(new List<ScriptableGroup>(){this}, type, ref currentIndex, targetIndex);
         }
 
         Object FlattenAndReturn(List<ScriptableGroup> parentList, Type type, ref int currentIndex, int targetIndex)
         {
             foreach (var element in ElementList)
             {
-                if (element == this)
-                    continue;
-                
                 if (element is ScriptableGroup nestedGroup)
                 {
                     if (parentList.Contains(nestedGroup))
                         continue;
                     
                     var newParentList = new List<ScriptableGroup>(parentList);
-                    newParentList.Add(this);
+                    newParentList.Add(nestedGroup);
                     var result = nestedGroup.FlattenAndReturn(newParentList, type, ref currentIndex, targetIndex);
                     if (result != null)
                         return result;
@@ -80,27 +73,23 @@ namespace PhEngine.QuickDropdown
         
         public override bool ContainsObject(Object targetObject)
         {
-            return targetObject != null && FlattenAndCheckContains(targetObject, new List<ScriptableGroup>());
+            return targetObject != null && FlattenAndCheckContains(targetObject, new List<ScriptableGroup>(){this});
         }
 
         bool FlattenAndCheckContains(Object targetToCheck, List<ScriptableGroup> parentList)
         {
             foreach (var element in ElementList)
             {
-                if (element == this)
-                    continue;
-                
                 if (element is ScriptableGroup nestedGroup)
                 {
                     if (parentList.Contains(nestedGroup))
                         continue;
                 
                     var newParentList = new List<ScriptableGroup>(parentList);
-                    newParentList.Add(this);
+                    newParentList.Add(nestedGroup);
                     if (nestedGroup.FlattenAndCheckContains(targetToCheck, newParentList))
                         return true;
                 }
-
                 if (element == targetToCheck)
                     return true;
             }
